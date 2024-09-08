d9fccad36532db48ea1721dbc67d638636ff5e99
rajat4984 <rs20021023@gmail.com>
2024-09-08 16:08:39 +0530

    updated uploaded function

--- server/api/upload.js
+++ server/api/upload.js
@@ -1,3 +1,57 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: "Hello, World!" });
+const  { GoogleAIFileManager } = require("@google/generative-ai/server")
+const {GoogleGenerativeAI} = require("@google/generative-ai")
+
+const multer = require("multer");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    upload.single("file")(
+      req,
+      res,
+      (async(err) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ error: "Fille upload error", details: err.message });
+        }
+        try {
+          const fileBuffer = req.file.buffer;
+          const fileName = req.file.originalName;
+          const mimeType = req.file.mimeType;
+
+          const fileManager = new GoogleAIFileManager(
+            "AIzaSyDmJgDAc15y-X8zv-1xW3die75T42Oh_0o");
+          const uploadResult = await fileManager.uploadFile(fileBuffer,{
+            mimeType,
+            displayName:fileName
+          })
+
+          const genAI = new GoogleGenerativeAI("AIzaSyDmJgDAc15y-X8zv-1xW3die75T42Oh_0o");
+          const model = genAI.getGenerativeModel({model:"gemini-1.5-flash"});
+          const result = await model.generateContent([
+            'tell me about this image',
+            {
+              fileData:{
+                fileUri:uploadResult.file.uri,
+                mimeType:uploadResult.file.mimeType
+              }
+            }
+          ])
+
+          res.status(200).json({
+            fileUri: uploadResult.file.uri,
+            result,
+          });
+        } catch (error) {
+          res.status(500).json({ error: 'Error processing file', details: error.message });
+        }
+      })
+    );
   }
+  else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
