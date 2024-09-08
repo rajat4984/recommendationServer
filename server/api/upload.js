b6fc85082260f8567fa6695b1bbadf05025ee538
rajat4984 <rs20021023@gmail.com>
2024-09-08 16:25:23 +0530

    changed to cloudinary

--- server/api/upload.js
+++ server/api/upload.js
@@ -1,5 +1,5 @@
-const  { GoogleAIFileManager } = require("@google/generative-ai/server")
-const {GoogleGenerativeAI} = require("@google/generative-ai")
+const { GoogleAIFileManager } = require("@google/generative-ai/server");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const multer = require("multer");
 
@@ -8,50 +8,53 @@ const upload = multer({ storage });
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    upload.single("file")(
-      req,
-      res,
-      (async(err) => {
-        if (err) {
-          return res
-            .status(500)
-            .json({ error: "Fille upload error", details: err.message });
-        }
-        try {
-          const fileBuffer = req.file.buffer;
-          const fileName = req.file.originalName;
-          const mimeType = req.file.mimeType;
+    upload.single("file")(req, res, async (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ error: "Fille upload error", details: err.message });
+      }
+      try {
+        const fileBuffer = req.file.buffer;
+        const fileName = req.file.originalName;
+        const mimeType = req.file.mimeType;
 
-          const fileManager = new GoogleAIFileManager(
-            "AIzaSyDmJgDAc15y-X8zv-1xW3die75T42Oh_0o");
-          const uploadResult = await fileManager.uploadFile(fileBuffer,{
+        const fileManager = new GoogleAIFileManager(
+          "AIzaSyDmJgDAc15y-X8zv-1xW3die75T42Oh_0o"
+        );
+        const uploadResult = await fileManager.uploadFile(
+          "https://res.cloudinary.com/dobzi0uvb/image/upload/v1725736184/wmpc3kb4yxlfvcgjwyxl.jpg",
+          {
             mimeType,
-            displayName:fileName
-          })
+            displayName: fileName,
+          }
+        );
 
-          const genAI = new GoogleGenerativeAI("AIzaSyDmJgDAc15y-X8zv-1xW3die75T42Oh_0o");
-          const model = genAI.getGenerativeModel({model:"gemini-1.5-flash"});
-          const result = await model.generateContent([
-            'tell me about this image',
-            {
-              fileData:{
-                fileUri:uploadResult.file.uri,
-                mimeType:uploadResult.file.mimeType
-              }
-            }
-          ])
+        const genAI = new GoogleGenerativeAI(
+          "AIzaSyDmJgDAc15y-X8zv-1xW3die75T42Oh_0o"
+        );
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const result = await model.generateContent([
+          "tell me about this image",
+          {
+            fileData: {
+              fileUri: uploadResult.file.uri,
+              mimeType: uploadResult.file.mimeType,
+            },
+          },
+        ]);
 
-          res.status(200).json({
-            fileUri: uploadResult.file.uri,
-            result,
-          });
-        } catch (error) {
-          res.status(500).json({ error: 'Error processing file', details: error.message });
-        }
-      })
-    );
-  }
-  else {
-    res.status(405).json({ error: 'Method not allowed' });
+        res.status(200).json({
+          fileUri: uploadResult.file.uri,
+          result,
+        });
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: "Error processing file", details: error.message });
+      }
+    });
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
